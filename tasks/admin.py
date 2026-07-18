8c8b5c6075448b6961ca10eac1e53903e3f4f324
Serhii <sergei.amvarcumov@gmail.com>
2026-07-18 20:39:19 +0200

    Configure Django admin panel for Tag and Task models

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Tag, Task
 
-# Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["content", "datetime", "deadline", "is_done"]
+    list_filter = ["is_done", "tags"]
+    search_fields = ["content"]
