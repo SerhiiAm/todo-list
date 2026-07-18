94196b5c18ae5609a1a3866afa5b87309d59a364
Serhii <sergei.amvarcumov@gmail.com>
2026-07-18 22:18:15 +0200

    Add Create, Update, and Delete views and routes for Tasks model

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from tasks.views import (
     IndexView, TagListView, TagCreateView,
-    TagUpdateView,TagDeleteView,
+    TagUpdateView, TagDeleteView, TaskCreateView,
+    TaskUpdateView, TaskDeleteView,
 
 )
 
@@ -13,5 +14,8 @@ urlpatterns = [
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
     path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 
 ]
