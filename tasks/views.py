89ced4f5f96e4dfbeb04dfa3d31149e8120c1fbd
Serhii <sergei.amvarcumov@gmail.com>
2026-07-18 22:50:52 +0200

    Implement Task CRUD and refactor IndexView to ListView

--- tasks/views.py
+++ tasks/views.py
@@ -3,23 +3,20 @@ from .models import Task, Tag
 from django.urls import reverse_lazy
 
 
-class IndexView(generic.TemplateView):
+class IndexView(generic.ListView):
+    model = Task
     template_name = "tasks/index.html"
+    context_object_name = "tasks_list"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
-        context["tasks_list"] = Task.objects.all()
-
         context["num_tags"] = Tag.objects.count()
         context["num_tasks"] = Task.objects.count()
-
         num_visits = self.request.session.get("num_visits", 0) + 1
         self.request.session["num_visits"] = num_visits
         context["num_visits"] = num_visits
 
         return context
-
 
 class TagListView(generic.ListView):
     model = Tag
@@ -46,3 +43,23 @@ class TagDeleteView(generic.DeleteView):
     model = Tag
     template_name = "tasks/tag_confirm_delete.html"
     success_url = reverse_lazy("tasks:tag-list")
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    template_name = "tasks/task_form.html"
+    success_url = reverse_lazy("tasks:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    template_name = "tasks/task_form.html"
+    success_url = reverse_lazy("tasks:index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    template_name = "tasks/task_confirm_delete.html"
+    success_url = reverse_lazy("tasks:index")
