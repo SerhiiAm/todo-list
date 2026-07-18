d7adb11e884317ab313444c9391c5683d4a0d641
Serhii <sergei.amvarcumov@gmail.com>
2026-07-18 20:56:15 +0200

    add IndexView  with task list and session counter

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.views import generic
+from .models import Task, Tag
 
-# Create your views here.
+
+class IndexView(generic.TemplateView):
+    template_name = "tasks/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["tasks_list"] = Task.objects.all()
+
+        context["num_tags"] = Tag.objects.count()
+        context["num_tasks"] = Task.objects.count()
+
+        num_visits = self.request.session.get("num_visits", 0) + 1
+        self.request.session["num_visits"] = num_visits
+        context["num_visits"] = num_visits
+
+        return context
