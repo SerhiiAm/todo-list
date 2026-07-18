e98209f11d18f5c6fe7e5f0a6a35c2b153670009
Serhii <sergei.amvarcumov@gmail.com>
2026-07-18 21:23:47 +0200

    Add tag-list route to urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from tasks.views import(
-    IndexView
+from tasks.views import (
+    IndexView, TagListView
 
 )
 
 app_name = "tasks"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
 
 ]
